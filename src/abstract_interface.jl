a1adec0e013e313a695abe69299273c91525a9fb
lucaferranti <lucaa.ferranti@gmail.com>
2025-05-10 00:53:34 +0300

    remove non-needed functions

--- src/abstract_interface.jl
+++ src/abstract_interface.jl
@@ -20,26 +20,6 @@ abstract type AbstractCut <: AbstractDedekindReal end
 Base.isfinite(::AbstractDedekindReal) = true
 Base.isnan(::AbstractDedekindReal) = false
 
-"Width of the current precise appoximation of t mosthe cut"
-width(c::AbstractDedekindReal) = width(DyadicInterval(c))
-
-"Midpoint of the current most precise approximation of the cut"
-midpoint(c::AbstractDedekindReal) = midpoint(DyadicInterval(c))
-
-"Width of the current precise appoximation of t mosthe cut"
-radius(c::AbstractDedekindReal) = radius(DyadicInterval(c))
-
-"Lower bound of the real number approximated by the cut"
-low(c::AbstractDedekindReal) = low(DyadicInterval(c))
-
-"Lower bound of the real number approximated by the cut"
-high(c::AbstractDedekindReal) = high(DyadicInterval(c))
-
-"Whether or not two cuts are overlapping in the current precision"
-function overlaps(c1::AbstractDedekindReal, c2::AbstractDedekindReal)
-    overlaps(DyadicInterval(c1), DyadicInterval(c2))
-end
-
 """
 Refine the given cut to give an approximation with `precision` bits of accuracy.
 If the required accuracy cannot be achieved within `max_iter` iterations, return the current estimate
